1adf047fdf7b982ee95183665cdfe463c662fe11
syoch <syoch64@gmail.com>
2025-05-11 06:27:48 +0000

    Update: Moved toolchain file to robotics image

--- cmake/config/ESP32.cmake
+++
@@ -1,9 +0,0 @@
-
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-
-set(SYOCH_ROBOTICS_TARGET "ESP" CACHE STRING "Thread implementation")
-set(EMULATION_ENABLED OFF CACHE STRING "Enable emulation")
-
-set(EXTRA_COMPONENT_DIRS "${CMAKE_SOURCE_DIR}/src-esp32")
-set(COMPONENTS esp32-rw)
-include($ENV{IDF_PATH}/tools/cmake/project.cmake)
