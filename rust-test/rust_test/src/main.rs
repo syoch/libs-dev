3efa55df8d264380850fab9a0532d1cf7351a1c7
syoch <syoch64@gmail.com>
2025-08-31 19:45:50 +0900

    Tests: IM920 library

--- rust-test/rust_test/src/main.rs
+++ rust-test/rust_test/src/main.rs
@@ -1,16 +1,12 @@
 extern crate im920_rs;
 extern crate srobo_base;
 
-use std::{
-    sync::{Arc, mpsc},
-    thread::sleep,
-    time::Duration,
-};
+use std::{thread::sleep, time::Duration};
 
 use im920_rs::Packet;
-use srobo_base::communication::{AsyncReadableStream, AsyncSerial, SerialDevice, WritableStream};
+use srobo_base::communication::{AsyncSerial, SerialDevice};
 
-fn _main_im920_test() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let dev = SerialDevice::new("/dev/ttyUSB1".to_string(), 19200);
     let (mut rx, mut tx) = dev.open().unwrap();
     let mut time = srobo_base::time::HostTime::new();
@@ -34,13 +30,13 @@ fn _main_im920_test() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     loop {
-        let lst = [0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80] as [u8; 8];
+        let buf = [0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80] as [u8; 8];
 
         sleep(Duration::new(1, 0));
         let tx_result = im920.transmit_broadcast(
             Packet {
                 node_id: 0,
-                data: &lst,
+                data: &buf,
             },
             Duration::new(1, 0),
         );
@@ -48,42 +44,5 @@ fn _main_im920_test() -> Result<(), Box<dyn std::error::Error>> {
             Ok(_) => println!("Transmitted broadcast packet"),
             Err(_) => println!("Failed to transmit broadcast packet"),
         };
-    }
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    loop {
-        {
-            let dev = Arc::new(SerialDevice::new("/dev/ttyACM1".to_string(), 921600));
-            let (mut rx, _tx) = dev.open().unwrap();
-            rx.on_data(Box::new(|data| {
-                let mut buffer = "".to_string();
-                for byte in data {
-                    let ch = *byte as char;
-
-                    if ch.is_ascii_graphic() {
-                        buffer.push(ch);
-                    } else {
-                        buffer.push_str(&format!("\\x{:02x}", *byte));
-                    }
-                }
-                println!("Received data: {}", buffer);
-            }))
-            .unwrap();
-
-            let (stop_tx, stop_rx) = mpsc::channel();
-
-            (|r: mpsc::Sender<_>| {
-                rx.on_closed(Box::new(move || {
-                    println!("Port Closed");
-                    r.send(()).unwrap();
-                }))
-                .unwrap()
-            })(stop_tx.clone());
-
-            println!("Waiting for stop");
-            stop_rx.recv().expect("Failed to receive stop signal");
-            println!("Stop received, exiting");
-        }
     }
 }
