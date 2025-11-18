16242826debe6807ede32921ecb40903a9000f01
syoch <syoch64@gmail.com>
2025-11-18 10:37:44 +0900

    Chore: Migrated Roboenv

--- flake.nix
+++ flake.nix
@@ -1,47 +1,35 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/25.05";
+
+  inputs.libs-cmake.url = "path:/mnt/data/ghq/github.com/syoch/libs-dev/libs/libs-cmake";
+  inputs.libs-cmake.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.static-mbed-os.url = "path:/mnt/data/ghq/github.com/syoch/libs-dev/libs/static-mbed-os";
+  inputs.static-mbed-os.inputs.libs-cmake.follows = "libs-cmake";
+  inputs.static-mbed-os.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.roboenv.url = "path:/mnt/data/ghq/github.com/syoch/libs-dev/libs/roboenv-nix";
+  inputs.roboenv.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.roboenv.inputs.libs-cmake.follows = "libs-cmake";
+  inputs.roboenv.inputs.static-mbed-os.follows = "static-mbed-os";
+
+  inputs.N25A.url = "path:/mnt/data/ghq/github.com/syoch/libs-dev/NHK2025A";
+  inputs.N25A.inputs.libs-cmake.follows = "libs-cmake";
+  inputs.N25A.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs =
-    { nixpkgs, ... }:
+    { roboenv, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import roboenv.inputs.nixpkgs { inherit system; };
+      rpkgs = roboenv.packages.x86_64-linux;
+      tpkgs = pkgs.callPackage ./tests {
+        inherit (roboenv.packages.x86_64-linux) static-mbed-os-f446re;
+        inherit (roboenv.lib) buildCMakeProject;
+      };
     in
-    rec {
-      packages.x86_64-linux.qemu-arm-xpack = pkgs.stdenv.mkDerivation {
-        name = "qemu-arm-xpack";
-        src = fetchTarball {
-          url = "https://github.com/xpack-dev-tools/qemu-arm-xpack/releases/download/v8.2.2-1/xpack-qemu-arm-8.2.2-1-linux-x64.tar.gz";
-          sha256 = "sha256:1vfd3r8cf4p3wnx6drwvmlb04i9k62jix8rv8ni1c517a66d5fv0";
-        };
-
-        installPhase = ''
-          mkdir -p $out
-          cp -r ./* $out/
-          chmod +x $out/bin/*
-        '';
-      };
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          cargo
-          rust-analyzer
-          pkg-config
-          udev
-          rustfmt
-          rustc
-          clippy
-
-          cmake
-          ccache
-          stlink-tool
-
-          git-conventional-commits
-
-          gcc-arm-embedded-14
-
-          packages.x86_64-linux.qemu-arm-xpack
-        ];
-        RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
-      };
+    {
+      packages.x86_64-linux = rpkgs // tpkgs;
+      devShells.x86_64-linux.default = roboenv.devShells.x86_64-linux.default;
     };
 }
